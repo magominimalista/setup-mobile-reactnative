9f820c51a1bd66964a3c15a6e62b79e7ef27b039
Philipe Cairon <contato@magominimalista.com.br>
2023-01-19 00:11:05 -0300

    Iniciando minha aplicação mobile

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <Loading />
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textview}>Eu, Philipe me comprometo a fazer de 2023 o melhor ano da minha vida.</Text>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
     </View>
   );
 }
@@ -13,8 +24,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
+    color: '#FFFFFF',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20
   },
+  textview: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    fontFamily: 'Inter_400Regular',
+    textAlign: 'center',
+    lineHeight: 34
+  }
 });
